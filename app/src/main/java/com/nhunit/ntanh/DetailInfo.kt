ba0bafaccdb2e8d18427445dcbe5bc1765e7916a
20041403 <tmtam.c3tqcap.a11@gmail.com>
2020-10-18 20:00:32 +0700

    code

---
+++ app/src/main/java/com/nhunit/ntanh/DetailInfo.kt
@@ -0,0 +1,12 @@
+package com.nhunit.ntanh
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailInfo: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_detail)
+    }
+}
