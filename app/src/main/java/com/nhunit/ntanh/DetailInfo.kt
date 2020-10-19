65b0a00d4d855b13259b979aa0a55287b76f1fea
akonho99qn <akonho99qn@gmail.com>
2020-10-19 11:02:06 +0700

    done

--- app/src/main/java/com/nhunit/ntanh/DetailInfo.kt
+++ app/src/main/java/com/nhunit/ntanh/DetailInfo.kt
@@ -3,10 +3,22 @@ package com.nhunit.ntanh
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class DetailInfo: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        var textName: String? = intent.getStringExtra("name")
+        Show_textName.text =textName.toString()
+        var textCMND: String? = intent.getStringExtra("cmnd")
+        Show_textCMND.text =textCMND.toString()
+        var textBC: String? = intent.getStringExtra("bangcap")
+        Show_rdo_bc.text = textBC.toString()
+        var textST: String? = intent.getStringExtra("sothich")
+        Show_rdo_sothich.text = textST.toString()
+        var textADD: String? =intent.getStringExtra("add")
+        Show_edit_text.text =textADD.toString()
     }
 }
--- app/src/main/java/com/nhunit/ntanh/MainActivity.kt
+++ app/src/main/java/com/nhunit/ntanh/MainActivity.kt
@@ -10,10 +10,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_send.setOnClickListener {
+//        btn_send.setOnClickListener {
+//            var intent = Intent(this,DetailInfo::class.java)
+//            this.startActivity(intent)
+//        }
+        btn_send.setOnClickListener() {
             var intent = Intent(this,DetailInfo::class.java)
+            intent.putExtra("cmnd",textCMND.text.toString())
+            intent.putExtra("name",textName.text.toString())
+            var btn_rdo = ""
+            if(rdo_bc.checkedRadioButtonId == rdo_tc.id){
+                btn_rdo = rdo_tc.text.toString()
+            }
+            else if (rdo_bc.checkedRadioButtonId == rdo_cd.id){
+                btn_rdo = rdo_cd.text.toString()
+            }
+            else if (rdo_bc.checkedRadioButtonId == rdo_dh.id){
+                btn_rdo = rdo_dh.text.toString()
+            }
+            intent.putExtra("bangcap",btn_rdo.toString())
+            var btn_cbx = ""
+            if (cbx_db.isChecked==true){
+                btn_cbx += cbx_db.text.toString()
+            }
+            if(cbx_ds.isChecked==true){
+                btn_cbx +=cbx_hcode.text.toString()
+            }
+            if(cbx_hcode.isChecked ==true){
+                btn_cbx += cbx_ds.text.toString()
+            }
+
+            intent.putExtra("sothich",btn_cbx.toString())
+            intent.putExtra("add",edit_add.text.toString())
             startActivity(intent)
+
         }
+
     }
 
 
